27ef21d2b28227ab93fd7d1884e645333985ea9c
Orlando <orlando.dan14@gmail.com>
2018-03-27 11:02:49 -0300

    Initialize Repository

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require plugins/jquery/jquery.min
+//= require plugins/jquery-ui/jquery-ui.min
+//= require plugins/bootstrap/js/bootstrap.min
+//= require plugins/smoothscroll/SmoothScroll.min
+//= require plugins/waypoints/waypoints.min
+//= require plugins/counter-up/jquery.counterup.min
+//= require plugins/datepicker/bootstrap-datepicker.min
+//= require plugins/selectbox/jquery.selectbox-0.1.3.min
+//= require plugins/owl-carousel/owl.carousel.min
+//= require plugins/isotope/isotope.min
+//= require plugins/fancybox/jquery.fancybox.pack
+//= require plugins/isotope/isotope-triger.min
+//= require plugins/map/js/rich-marker
+//= require plugins/map/js/infobox_packed
+//= require single-map
+//= require map
+//= require custom
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
